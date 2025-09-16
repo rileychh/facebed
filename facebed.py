0d524193181768b0928207c5aa7f648e4dd61a08
pie <naivenevaii@gmail.com>
2025-09-16 21:55:59 +0700

    updated headers pt2

--- facebed.py
+++ facebed.py
@@ -16,7 +16,7 @@ from urllib.parse import quote as _quote_
 from html import escape
 from urllib.parse import urlparse
 
-import requests
+import stealth_requests as requests
 import yaml
 from bottle import Bottle, request, response, static_file
 from bs4 import BeautifulSoup
@@ -304,20 +304,12 @@ class JsonParser:
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/jxl,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-language': 'en-US,en;q=0.9',
             'cache-control': 'no-cache',
-            'dnt': '1',
             'pragma': 'no-cache',
             'priority': 'u=0, i',
-            'sec-ch-ua': '"Not?A_Brand";v="99", "Chromium";v="130"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
             'sec-fetch-mode': 'navigate',
             'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'sec-gpc': '1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
         }
+
         return headers
 
 
